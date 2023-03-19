c7446060c72fb70f19f66166c171e95d24761f0e
Dihero86 <diegohrodriguez86@gmail.com>
2023-03-19 09:20:52 +0000

    feat: First Version

---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,32 @@
+import React, {useContext} from "react";
+import starimages from "../../img/pngwing.com.png";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+
+const Navbar = () => {
+
+	const {store,actions}=useContext(Context);
+
+	return (
+		<nav className="navbar navbar-light bg-light mb-3 px-5">
+			<Link to="/">
+				<span className="navbar-brand mb-0 h1"><img src={starimages} height={'150rem'}/></span>
+			</Link>
+			<div className="btn-group ">
+				<button type="button" className="d-flex btn btn-primary dropdown-toggle align-items-center justify-content-around"  style={{width:'200px', fontSize:'27px'}} data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+					<div>Favorites </div><div className="bg-secondary m-1" style={{width:'55px'}}>{store.favorites.length}</div>
+				</button>
+				<ul className="dropdown-menu dropdown-menu-lg-end" style={{fontSize:'20px'}}>
+					{store.favorites.length===0 ? <h5>'empty'</h5> : store.favorites.map((name,index)=>
+						<li className="d-flex justify-content-between p-2" key={index}>
+							{name} <a onClick={()=>actions.deleteLikes(index)} className="btn btn-outline-primary">X</a>
+						</li>)
+					}
+				</ul>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
--- src/js/layout.js
+++ src/js/layout.js
@@ -2,13 +2,12 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+import Home from "./views/home.jsx";
+import Detail from "./views/detail.jsx";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+import Navbar from "./component/navbar.jsx";
+
 
 //create your first component
 const Layout = () => {
@@ -17,17 +16,15 @@ const Layout = () => {
 	const basename = process.env.BASENAME || "";
 
 	return (
-		<div>
+		<div className="container-fluid">
 			<BrowserRouter basename={basename}>
 				<ScrollToTop>
-					<Navbar />
+				<Navbar />
 					<Routes>
 						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
+						<Route path="/:type/:id" element={<Detail />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
-					<Footer />
 				</ScrollToTop>
 			</BrowserRouter>
 		</div>
